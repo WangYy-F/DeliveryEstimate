7a39023e77c2525c9a5a77670d9d617a754cf0c1
Wang Yangyang <wangyangyang@sjtu.edu.cn>
2023-09-29 14:26:57 -0400

    update area selection

--- script.js
+++ script.js
@@ -5,13 +5,22 @@ let buttonStatus = ["T", "T", "T", "T", "T", "T"];
 function toggleButtonStatus(buttonId) {
     const index = parseInt(buttonId.replace("button", "")) - 1;
     buttonStatus[index] = buttonStatus[index] === "T" ? "F" : "T";
-    updateResultFromCSV(buttonStatus);
+    updateResult();
 }
 
-// Function to update the result based on the recorded data from CSV
-function updateResultFromCSV(buttonStates) {
-    // Replace with the path to your CSV file
-    const csvFilePath = 'data.csv';
+// Function to update the result based on the selected geographical area and button states
+function updateResult() {
+    // Get the selected area from the dropdown
+    const selectedArea = document.getElementById('area-select').value;
+
+    // Define a mapping of area values to CSV file paths
+    const areaToCsvPath = {
+        DFW: 'data/dfw.csv',
+        NY: 'data/ny.csv',
+        // Add mappings for other areas as needed
+    };
+
+    const csvFilePath = areaToCsvPath[selectedArea];
 
     fetch(csvFilePath)
         .then(response => response.text())
@@ -25,7 +34,7 @@ function updateResultFromCSV(buttonStates) {
                 const rowButtonStates = rowValues.slice(0, 6); // Extract button states from the row
                 const result = rowValues[6]; // Extract the result
 
-                if (JSON.stringify(rowButtonStates) === JSON.stringify(buttonStates)) {
+                if (JSON.stringify(rowButtonStates) === JSON.stringify(buttonStatus)) {
                     document.getElementById("value").textContent = result;
                     return; // Exit the loop if a match is found
                 }
@@ -41,8 +50,13 @@ for (let i = 1; i <= 6; i++) {
     button.addEventListener("click", () => {
         toggleButtonStatus(buttonId);
         button.textContent = buttonStatus[i - 1];
+        updateResult(); // Update the result when a button is clicked
     });
 }
+
+// Add an event listener to the area select dropdown
+const areaSelect = document.getElementById('area-select');
+areaSelect.addEventListener('change', updateResult); // Update the result when the area is changed
 
 // Initialize button status and update result
 document.addEventListener("DOMContentLoaded", () => {
@@ -50,9 +64,10 @@ document.addEventListener("DOMContentLoaded", () => {
         const buttonId = `button${i}`;
         const textField = document.getElementById(`text${i}`);
         const buttonText = textField.value;
-        button.textContent = buttonText;
+        const button = document.getElementById(buttonId); // Define the button here
+        button.textContent = buttonStatus[i-1];
     }
 
     // Initial update of result
-    updateResultFromCSV(buttonStatus);
+    updateResult();
 });
